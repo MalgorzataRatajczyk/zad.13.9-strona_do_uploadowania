fa687ed3c56ff04e50a0923f3f2f4576ca19ca89
Malgorzata Ratajczyk <malgorzataratajczyk@wp.pl>
2018-10-07 22:28:48 +0200

    Change handlers.js

--- modules/handlers.js
+++ modules/handlers.js
@@ -17,17 +17,10 @@ exports.upload = function(request, response) {
 
 exports.welcome = function(request, response) {
     console.log("Rozpoczynam obsługę żądania welcome.");
-    fs.readFile('../templates/start.html', function(err, html) {
+    fs.readFile('templates/start.html', function(err, html) {
         response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-        response.write(html);
-        response.end();
-    });
-}
-
-exports.show = function(request, response) {
-    fs.readFile("test.png", "binary", function(error, file) {
-        response.writeHead(200, {"Content-Type": "image/png"});
-        response.write(file, "binary");
+        var test = toString(html);
+        response.write(test);
         response.end();
     });
 }
@@ -38,4 +31,10 @@ exports.error = function(request, response) {
     response.end();
 }
 
-
+exports.show = function(request, response) {
+    fs.readFile("test.png", "binary", function(error, file) {
+        response.writeHead(200, {"Content-Type": "image/png"});
+        response.write(file, "binary");
+        response.end();
+    });
+}
